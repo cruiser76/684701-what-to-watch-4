722a816aaff8eaba547e10089314afaac34d1c19
Aleksandr Leontev <cruiser76_git@mail.ru>
2020-06-16 13:41:55 +0600

    Добавляет PropTypes компонентам

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Main from './../main/main.jsx';
 
+const GENRES_LIST = [
+  `Comedie`, `Crime`, `Documentary`, `Drama`, `Horror`, `Kids & Family`, `Romance`, `Sci-Fi`, `Thriller`
+];
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {movieTitle, genre, releaseDate} = props;
+  const {movieTitle, genre, releaseDate, movies} = props;
 
   return (
     <Main
       movieTitle={movieTitle}
       genre={genre}
       releaseDate={releaseDate}
+      movies = {movies}
     />
   );
+};
+
+App.propTypes = {
+  movieTitle: PropTypes.string.isRequired,
+  genre: PropTypes.oneOf(GENRES_LIST).isRequired,
+  releaseDate: PropTypes.number.isRequired,
+  movies: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default App;
