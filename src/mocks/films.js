fcdfd952dd3d09381975838d738a82a828dd7b11
Aleksandr Leontev <cruiser76_git@mail.ru>
2020-06-24 10:50:09 +0600

    добавляет моки

---
+++ src/mocks/films.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    src: `fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+    link: `movie-page.html`
+  }, {
+    src: `bohemian-rhapsody.jpg`,
+    title: `Bohemian Rhapsody`,
+    link: `movie-page.html`
+  }, {
+    src: `macbeth.jpg`,
+    title: `Macbeth`,
+    link: `movie-page.html`
+  }, {
+    src: `aviator.jpg`,
+    title: `Aviator`,
+    link: `movie-page.html`
+  }, {
+    src: `we-need-to-talk-about-kevin.jpg`,
+    title: `We need to talk about Kevin`,
+    link: `movie-page.html`
+  }, {
+    src: `what-we-do-in-the-shadows.jpg`,
+    title: `What We Do in the Shadows`,
+    link: `movie-page.html`
+  }, {
+    src: `revenant.jpg`,
+    title: `Revenant`,
+    link: `movie-page.html`
+  }, {
+    src: `johnny-english.jpg`,
+    title: `Johnny English`,
+    link: `movie-page.html`
+  }
+];
