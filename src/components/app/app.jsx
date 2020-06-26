1b56ba27d10b81a7796cacd9b2dc719c43f0dc70
Aleksandr Leontev <cruiser76_git@mail.ru>
2020-06-26 22:19:15 +0600

    Добавляет роутер и страницу фильма

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Main from './../main/main.jsx';
+import MoviePage from './../movie-page/movie-page.jsx';
 
 const movieCardTitleClickHandle = () => {};
 
 const App = (props) => {
 
-  return (
-    <Main
+  function _renderApp() {
+    return (<Main
       {...props}
       onMovieCardTitleClick={movieCardTitleClickHandle}
-    />
+    />);
+  }
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          {_renderApp()}
+        </Route>
+        <Route exact path="/dev-component">
+          <MoviePage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
