6efae6da2c5953b4899f7f148a68aeaf413633a2
Aleksandr Leontev <cruiser76_git@mail.ru>
2020-06-27 20:53:02 +0600

    Добавляет снепшот тест на компонент MoviePage

---
+++ src/components/movie-page/movie-page.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import MoviePage from './movie-page.jsx';
+
+const movie =
+{
+  img: {
+    src: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    posterSrc: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    bgSrc: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`
+  },
+  brief: {
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+    genre: `Drama`,
+    year: `2000`,
+    score: `9`,
+    level: `Good`
+  },
+  link: `movie-page.html`,
+  key: `fantastic-beasts-the-crimes-of-grindelwald`,
+};
+
+it(`Movie Page is render correctly`, () => {
+  const tree = renderer
+    .create(
+        <MoviePage
+          movie={movie}
+        />
+    ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
